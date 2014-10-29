7a521c64e0c79101326399cf0a9dc158cc819ac5
Akshay Dixit <akshaydixi@gmail.com>
2014-10-29 22:50:05 +0530

    refactored Sudoku.scala, added block validator and added random generator in test

--- src/main/scala/sudoku4sc/Sudoku.scala
+++ src/main/scala/sudoku4sc/Sudoku.scala
@@ -3,16 +3,28 @@ package sudoku4sc
 import scala.io.Source
 
 class Sudoku(val board: Vector[Vector[Int]]) {
-
+  val ALL_INTS = Vector(0,1,2,3,4,5,6,7,8,9)
+  type Block = Vector[Int]
+  // Constructor to read a Sudoku board from a CSV file
   def this(fileName: String) {
     this(Source.fromFile(fileName).getLines().toVector.map(line => line.split(',').map(x => x.toInt).toVector))
   }
-  def isSudoku: Boolean = board.length == 9 && board.map( x => x.length == 9 && x.exists(Vector(0,1,2,3,4,5,6,7,8,9).contains(_))).foldLeft(true)( (x,y) => x & y)
 
+  // To check if a Sudoku board is valid
+  def isSudoku: Boolean = board.length == 9 && board.map( x => x.length == 9 && x.forall(ALL_INTS.contains(_))).reduce(_ & _)
+
+  // To check if a Sudoku has been solved -> Simply check if there aren't any zeros
   def isSolved: Boolean = {
-    !board.map( x => x.contains(0)).foldLeft(false)( (x,y) => x | y)
+    !board.map(_.contains(0)).reduce(_ | _)
   }
 
+  // To check if a Block( row, column or a (3 x 3) block is valid -> Simply check if all numbers appear once
+  def isValidBlock(block: Block): Boolean = {
+    val blockWithoutZero = block.filter(_ > 0)
+    blockWithoutZero.distinct == blockWithoutZero && block.length == 9
+  }
+
+  // To show Sudoku boards cleanly
   override def toString: String = {
     val str = for (row <- board) yield row.mkString(" ")
     str.mkString("\n", "\n", "\n")
--- src/test/scala/sudoku4sc/SudokuTest.scala
+++ src/test/scala/sudoku4sc/SudokuTest.scala
@@ -2,12 +2,24 @@ package sudoku4sc
 
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
+import scala.util.Random
 
 class SudokuTest extends FlatSpec with ShouldMatchers {
   val unsolvedBoard = new Sudoku("src/main/resources/unsolved1.csv")
   val solvedBoard = new Sudoku("src/main/resources/solved1.csv")
   val emptyBoard = new Sudoku("src/main/resources/empty.csv")
   val disfiguredBoard = new Sudoku("src/main/resources/disfigured1.csv")
+  val random = new Random(System.currentTimeMillis)
+
+  def cell: Int = {
+    if (random.nextInt(100) > 10) random.nextInt(9) + 1
+    else 0
+  }
+
+  def arbitrarySudoku: Sudoku = {
+    val randomBoard = Vector.fill(9,9)(cell)
+    new Sudoku(randomBoard)
+  }
 
   "Sudoku" should "read from csv files properly" in {
     emptyBoard.board should be (Main.allBlankSudoku.board)
@@ -17,6 +29,7 @@ class SudokuTest extends FlatSpec with ShouldMatchers {
     unsolvedBoard.isSudoku should be (true)
     solvedBoard.isSudoku should be (true)
     disfiguredBoard.isSudoku should be (false)
+    arbitrarySudoku.isSudoku should be (true)
   }
 
   "Sudoku" should "implement isSolved properly" in {
@@ -24,5 +37,10 @@ class SudokuTest extends FlatSpec with ShouldMatchers {
     solvedBoard.isSolved should be (true)
   }
 
+  "Sudoku" should "implement isValidBlock properly" in {
+    solvedBoard.isValidBlock(solvedBoard.board.head) should be (true)
+    unsolvedBoard.isValidBlock(unsolvedBoard.board.head) should be (true)
+    emptyBoard.isValidBlock(Vector(1,2,1,1,2,3,4,5,6)) should be (false)
+  }
 
 }
